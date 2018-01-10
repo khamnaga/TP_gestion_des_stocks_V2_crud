eed255ae5a5785ba88f35682cad38fd2232f0465
Richard Phonthibsvads <phs.richards@gmail.com>
2018-01-10 11:29:29 +0100

    maj dossiers

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<?php include_once dirname(__FILE__) . "./../crud.php";?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>BIZONLINE V2</title>
+        <base href="<?php echo $base_url ?>">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles/css/styles.css">
+        <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+        <script src="modules/products/products.js"></script>
+    </head>
+    <body>
+        <h1 class="title">
+            <a href="index.php">Gestion des Stocks</a>
+        </h1>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "includes/head.php"; ?>
+
+<h2 class="title">Products list</h2>
+<?php include "modules/products/formulaire.html"; ?>
+
+<?php include "includes/footer.php"; ?>
